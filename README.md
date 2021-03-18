fbf1c7186ad65ec323b0e750c55a3f78823cef92
Dylan Gore <hello@dylangore.ie>
2021-03-18 13:34:51 +0000

    :bulb: Add missing comment and general code cleanup

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # micro:bit BLE Vue
 
+A Vue.JS web app that communicates with a [micro:bit](https://microbit.org/) using Bluetooth Low Energy (BLE) and the [Web Bluetooth API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API).
+
 ## Development
 
 Install dependencies:
@@ -13,3 +15,9 @@ Run development server:
 ```bash
 npm run serve
 ```
+
+**Note:** I would recommend using something like [ngrok](https://ngrok.com/) to get an easy HTTPS endpoint.
+
+## Thanks
+
+Based on code from [https://github.com/ngammarano/microbit-web-bluetooth](https://github.com/ngammarano/microbit-web-bluetooth) and [https://github.com/thegecko/microbit-web-bluetooth](https://github.com/thegecko/microbit-web-bluetooth).
--- src/components/Microbit.vue
+++ src/components/Microbit.vue
@@ -1,49 +1,61 @@
 <template>
   <h2>Connection</h2>
+
+  <!-- Connection buttons -->
   <div class="btn-group d-grid gap-2" role="group" aria-label="micro:bit controlls">
+    <!-- Display only if the microbit is NOT already connected -->
     <button v-if="!microbit.isConnected" type="button" class="btn btn-success" @click="connectMicrobit()">Connect to micro:bit</button>
+    <!-- Display only when the micro:bit IS already connected -->
     <button v-if="microbit.isConnected" type="button" class="btn btn-danger" @click="disconnectMicrobit()">Disconnect</button>
   </div>
-  <div id="microbit-info" v-if="this.microbit.isConnected">
+
+  <!-- Only display this entire block if there is a micro:bit connected -->
+  <div v-if="microbit.isConnected" id="microbit-info">
+    <!-- Display the raw accelerometer data in text -->
     <h3>Accelerometer</h3>
     <p>
       Raw Data:
       <br />
       {{ microbit.accelerometer }}
     </p>
+
+    <!-- Display the accelerometer using progress bars -->
     <div class="progress my-2">
       <div class="progress-bar" role="progressbar" :style="'width: ' + xPercent + '%'" :aria-valuenow="xPercent" aria-valuemin="0" aria-valuemax="100">
-        x: {{ this.microbit.accelerometer.x }}
+        x: {{ microbit.accelerometer.x }}
       </div>
     </div>
     <div class="progress my-2">
       <div class="progress-bar" role="progressbar" :style="'width: ' + yPercent + '%'" :aria-valuenow="yPercent" aria-valuemin="0" aria-valuemax="100">
-        y: {{ this.microbit.accelerometer.y }}
+        y: {{ microbit.accelerometer.y }}
       </div>
     </div>
     <div class="progress my-2">
       <div class="progress-bar" role="progressbar" :style="'width: ' + zPercent + '%'" :aria-valuenow="zPercent" aria-valuemin="0" aria-valuemax="100">
-        z: {{ this.microbit.accelerometer.z }}
+        z: {{ microbit.accelerometer.z }}
       </div>
     </div>
 
+    <!-- Audio controls -->
     <h3>Audio</h3>
-    <audio controls id="audio" loop autoplay>
+    <audio id="audio" controls loop autoplay>
       <source src="https://ia802707.us.archive.org/25/items/win3.0midi/PASSPORT.ogg" type="audio/ogg" />
       Not supported
     </audio>
+
+    <!-- Display the playback rate variables -->
     <p>Playback Rate: {{ playbackRate }}</p>
     <p>Adjusted Rate: {{ adjustedPlaybackRate }}</p>
   </div>
 </template>
 
 <script>
-import * as Tone from 'tone';
-
 export default {
   name: 'Microbit',
   data() {
     return {
+      playbackRate: 1.0,
+      adjustedPlaybackRate: 1.0,
       xPercent: 50,
       yPercent: 50,
       zPercent: 50,
@@ -115,134 +127,182 @@ export default {
     };
   },
   methods: {
+    /**
+     * Event handler that runs whenever new accelerometer data is received
+     */
     accelerometerDataChanged(event) {
+      // Convert each data point into standard numbers and store in a variable
       this.microbit.accelerometer.x = event.target.value.getInt16(0, true); // Little Endian
       this.microbit.accelerometer.y = event.target.value.getInt16(2, true); // Little Endian
       this.microbit.accelerometer.z = event.target.value.getInt16(4, true); // Little Endian
 
+      // Log the accelerometer data to the browser console
       console.log({ x: this.microbit.accelerometer.x, y: this.microbit.accelerometer.y, z: this.microbit.accelerometer.z });
 
+      // Get the HTML5 audio element
+      let audio = document.getElementById('audio');
+
+      /** Count each accelerometer data entry in blocks of 50.
+       *  (This is used to modify the playback rate so it only changes on every 50th data point
+       * so it's not jumping around too much)
+       */
       if (this.microbit.accelerometer.count == 0) {
+        // Reset the counter back to 50
         this.microbit.accelerometer.count = 50;
+
+        /** Convert the playback rate to a percentage (between 0 and 1) by dividing it's
+         * absoulte value (remove the minus sign if it exists) by the maximum value (which is 2048).
+         */
+        this.playbackRate = Math.abs(this.microbit.accelerometer.y / 2048);
+
+        // Adjust the playback rate to specific values
+        if (this.playbackRate <= 0.25) {
+          // minimum playback rate
+          this.adjustedPlaybackRate = 0.25;
+        } else if (this.playbackRate >= 0.48 && this.playbackRate < 0.8) {
+          // Normal playback rate
+          this.adjustedPlaybackRate = 1.0;
+        } else if (this.playbackRate >= 0.8) {
+          // Fast playback rate
+          this.adjustedPlaybackRate = 1.5;
+        } else {
+          // Use the original playback rate
+          this.adjustedPlaybackRate = this.playbackRate;
+        }
+
+        // Set the playback rate of the HTML audio element
+        audio.playbackRate = this.adjustedPlaybackRate;
       } else {
+        // Decrement the counter by 1
         this.microbit.accelerometer.count--;
       }
 
-      let audio = document.getElementById('audio');
-
-      // switch(M)
-
-      if (this.microbit.accelerometer.count == 0) {
-        this.playbackRate = Math.abs(this.microbit.accelerometer.y / 2048);
-
-        if (this.playbackRate <= 0.25) {
-          this.adjustedPlaybackRate = 0.25;
-        } else if (this.playbackRate >= 0.48 && this.playbackRate < 0.8) {
-          this.adjustedPlaybackRate = 1.0;
-        } else if (this.playbackRate >= 0.8) {
-          this.adjustedPlaybackRate = 1.5;
-        } else {
-          this.adjustedPlaybackRate = this.playbackRate;
-        }
-        audio.playbackRate = this.adjustedPlaybackRate;
-      }
-
-      // Progress Bars
+      /**
+       * Set the value of the three progress bars, add 2048 to exclude negative values
+       * and convert to a percentage.
+       */
       this.xPercent = ((this.microbit.accelerometer.x + 2048) / 4096) * 100;
       this.yPercent = ((this.microbit.accelerometer.y + 2048) / 4096) * 100;
       this.zPercent = ((this.microbit.accelerometer.z + 2048) / 4096) * 100;
     },
+
+    /**
+     * Method to handle connecting to the micro:bit using Bluetooth Low Energy (BLE) and
+     * web bluetooth.
+     */
     async connectMicrobit() {
-      console.log('Requesting micro:bit Bluetooth devices... ', false);
+      console.log('Requesting micro:bit Bluetooth devices... ');
+
+      // Create a list of all possible micro:bit service UUIDs
+      let microbitOptionalServices = [
+        this.microbitUuid.genericAccess[0],
+        this.microbitUuid.genericAttribute[0],
+        this.microbitUuid.deviceInformation[0],
+        this.microbitUuid.accelerometerService[0],
+        this.microbitUuid.magnetometerService[0],
+        this.microbitUuid.buttonService[0],
+        this.microbitUuid.ioPinService[0],
+        this.microbitUuid.ledService[0],
+        this.microbitUuid.eventService[0],
+        this.microbitUuid.dfuControlService[0],
+        this.microbitUuid.temperatureService[0],
+        this.microbitUuid.uartService[0]
+      ];
+
+      // Check if the bluetooth adapter exists and is accessible
       if (!navigator.bluetooth) {
         console.error('Bluetooth adapter not available');
       } else {
+        // Search for micro:bit devices
         navigator.bluetooth
           .requestDevice({
             filters: [{ namePrefix: 'BBC micro:bit' }],
-            optionalServices: [
-              this.microbitUuid.genericAccess[0],
-              this.microbitUuid.genericAttribute[0],
-              this.microbitUuid.deviceInformation[0],
-              this.microbitUuid.accelerometerService[0],
-              this.microbitUuid.magnetometerService[0],
-              this.microbitUuid.buttonService[0],
-              this.microbitUuid.ioPinService[0],
-              this.microbitUuid.ledService[0],
-              this.microbitUuid.eventService[0],
-              this.microbitUuid.dfuControlService[0],
-              this.microbitUuid.temperatureService[0],
-              this.microbitUuid.uartService[0]
-            ]
+            optionalServices: microbitOptionalServices
           })
           .then((device) => {
-            console.log("<font color='green'>OK</font>", true);
+            // Successful connection
+            console.log('Connection successful!');
+            // Store the bluetooth device object in a variable
             this.microbit.device = device;
-            console.log("Connecting to GATT server (name: <font color='blue'>" + device.name + "</font>, ID: <font color='blue'>" + device.id + '</font>)... ', false);
+            // Add an event listener to run on disconnect
             device.addEventListener('gattserverdisconnected', this.onDisconnected);
+            // Initiate the connection
             return device.gatt.connect();
           })
           .then((server) => {
+            // Set the isConnected variable to true
             this.microbit.isConnected = true;
-            console.log("<font color='green'>OK</font>", true);
-            console.log('Getting Accelerometer service (UUID: ' + this.microbitUuid.accelerometerService[0] + ')... ', false);
+            console.log('Getting Accelerometer service (UUID: ' + this.microbitUuid.accelerometerService[0] + ')... ');
+            // Get the accelerometer service (if it's not enabled, the error will be caught below)
             return server.getPrimaryService(this.microbitUuid.accelerometerService[0]);
           })
           .then((service) => {
-            console.log("<font color='green'>OK</font>", true);
-            console.log('Getting Accelerometer data characteristic... ', false);
+            console.log('Getting Accelerometer data characteristic... ');
+            // Setup the accelerometer receiver
             service
               .getCharacteristic(this.microbitUuid.accelerometerData[0])
               .then((dataChar) => {
-                console.log("<font color='green'>OK</font>", true);
                 this.microbit.accelerometerDataCharacteristic = dataChar;
-                console.log('Starting accelerometer data notifications... ', false);
+                console.log('Starting accelerometer data notifications... ');
                 return dataChar
                   .startNotifications()
                   .then(() => {
-                    console.log("<font color='green'>OK</font>", true);
+                    // Add an event listener to run whenever new accelerometer data is received
                     dataChar.addEventListener('characteristicvaluechanged', this.accelerometerDataChanged);
-                    console.log('Getting Accelerometer period characteristic... ', false);
-                    service
-                      .getCharacteristic(this.microbitUuid.accelerometerPeriod[0])
-                      .then((periodChar) => {
-                        this.microbit.accelerometerPeriodCharacteristic = periodChar;
-                        console.log("<font color='green'>OK</font>", true);
-                      })
-                      .catch((error) => {
-                        console.error(error);
-                      });
+                    // Get the acclerometer period (currently not used)
+                    // console.log('Getting Accelerometer period characteristic... ', false);
+                    // service
+                    //   .getCharacteristic(this.microbitUuid.accelerometerPeriod[0])
+                    //   .then((periodChar) => {
+                    //     this.microbit.accelerometerPeriodCharacteristic = periodChar;
+                    //     console.log("<font color='green'>OK</font>", true);
+                    //   })
+                    //   .catch((error) => {
+                    //     console.error(error);
+                    //   });
                   })
                   .catch((error) => {
+                    // Device not found or no bluetooth adapter
                     console.error(error);
                   });
               })
               .catch((error) => {
+                // Error while connecting to device
                 console.error(error);
               });
           })
           .catch((error) => {
+            // Error whule getting accelerometer data
             console.error(error);
           });
       }
     },
+
+    /**
+     * Method to disconnect the micro:bit BLE connection
+     */
     disconnectMicrobit() {
       console.log('Disconnecting... ');
+      // Ensure the device exists before attemting to disconnect
       if (!this.microbit.device) {
         console.error('There is no device connected.');
       } else {
+        // Ensure the stored device object is actually connected
         if (this.microbit.device.gatt.connected) {
+          // Disconnect the Bluetooth connection
           this.microbit.device.gatt.disconnect();
-          if (!this.microbit.device.gatt.connected) {
-            console.log("<font color='green'>OK</font>", true);
-          }
         } else {
           console.error('There is no device connected.');
         }
       }
     },
+
+    /**
+     * Event handler to run when a BLE device is disconnected
+     */
     onDisconnected() {
       console.log('BLE Disconnect!');
+      // Update the isConnected varaible to ensure the UI updates
       this.microbit.isConnected = false;
     }
   }
